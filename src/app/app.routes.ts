2a628ba3af5fd2459b5eb50dbf5522d75ed6f2c9
Tasos Antoniou <tassant@hotmail.com>
2026-09-05 16:21:30 +0300

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {App} from './app';
+import {Countries} from './pages/countries/countries';
+import {GdpPerformance} from './pages/gdp-performance/gdp-performance';
+import {Statistics} from './pages/statistics/statistics';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path:'/home', component: App},
+  { path:'/countries', component: Countries},
+  { path:'/gdp-performance', component: GdpPerformance},
+  { path:'/statistics', component: Statistics},
+];
