fa18bedca5560ed9cad38807f7bec1ba45cfacda
Tasos Antoniou <tassant@hotmail.com>
2026-09-05 20:34:28 +0300

    added home component and modified routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
-import {App} from './app';
+import {HomePageComponent} from './pages/home/home-page.component';
 import {CountriesPageComponent} from './pages/countries/countries-page.component';
 import {GdpPerformancePageComponent} from './pages/gdp-performance/gdp-performance-page.component';
 import {StatisticsPageComponent} from './pages/statistics/statistics-page.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path:'/home', component: App},
-  { path:'/countries', component: CountriesPageComponent},
-  { path:'/gdp-performance', component: GdpPerformancePageComponent},
-  { path:'/statistics', component: StatisticsPageComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent},
+  { path: 'countries', component: CountriesPageComponent},
+  { path: 'gdp-performance', component: GdpPerformancePageComponent},
+  { path: 'statistics', component: StatisticsPageComponent},
 ];
---
+++ src/app/pages/home/home-page.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-home-page',
+  styleUrl: './home-page.component.css',
+  templateUrl: './home-page.component.html',
+})
+export class HomePageComponent {}
