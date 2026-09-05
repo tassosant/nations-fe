e5e4b0220e37f7d0a1bbd0b0e350ed618b8e04d5
Tasos Antoniou <tassant@hotmail.com>
2026-09-05 20:22:37 +0300

    added some html elements

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Nav} from './components/nav/nav';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Nav],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
